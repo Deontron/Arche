ba71fc497274d6137ae595bdf22af3a1cfb3b3c8
Ahmet Çelik <ahmet.celik2605@outlook.com>
2023-01-30 23:18:04 +0300

    Player

--- Arche/Assets/Scripts2/PlayerScript.cs
+++ Arche/Assets/Scripts2/PlayerScript.cs
@@ -7,9 +7,13 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private GameObject[] rootParts;
     [SerializeField] private float speed;
 
+    private float rootTimer;
+    private float rootSpawnRate = 0.3f;
+    private int rootId = 0;
+
     void Start()
     {
-
+        StartCoroutine(RootTimer());
     }
 
     void Update()
@@ -17,8 +21,29 @@ public class PlayerScript : MonoBehaviour
         PlayerMovement();
     }
 
+    private void SetRoots()
+    {
+        rootParts[rootId].transform.position = transform.position;
+        rootParts[rootId].transform.rotation = transform.rotation;
+
+        rootId++;
+
+        if (rootId >= rootParts.Length)
+        {
+            rootId = 0;
+        }
+    }
+
     private void PlayerMovement()
     {
         transform.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed, -speed);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Input.GetAxis("Horizontal") * 45));
+    }
+
+    IEnumerator RootTimer()
+    {
+        yield return new WaitForSeconds(rootSpawnRate);
+        StartCoroutine(RootTimer());
+        SetRoots();
     }
 }
