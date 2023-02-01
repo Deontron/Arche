4bb425c3be319e3d071f7a49c64d4b932a5d149d
Ahmet Çelik <ahmet.celik2605@outlook.com>
2023-02-01 10:40:58 +0300

    insect damage

--- Arche/Assets/Scripts2/InsectScript.cs
+++ Arche/Assets/Scripts2/InsectScript.cs
@@ -6,7 +6,9 @@ using static UnityEngine.GraphicsBuffer;
 public class InsectScript : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float damage;
 
+    private GameManager gm;
     private GameObject player;
     void Start()
     {
@@ -23,5 +25,21 @@ public class InsectScript : MonoBehaviour
         Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position, transform.TransformDirection(-Vector3.forward));
         transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
         GetComponent<Rigidbody2D>().MovePosition(new Vector2(Mathf.Lerp(transform.position.x, player.transform.position.x, speed), transform.position.y));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (!collision.GetComponent<PlayerScript>().isShieldActive)
+            {
+                gm.GetDamage(damage);
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                collision.GetComponent<PlayerScript>().DeactivateShield();
+            }
+        }
     }
 }
